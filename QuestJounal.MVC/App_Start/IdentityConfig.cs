f9275124b6704cc63875c1b558b787857384c065
Jeffrey Hackett <Jeffth92@gmail.com>
2021-10-28 18:08:21 -0400

    nuget nugot

--- QuestJounal.MVC/App_Start/IdentityConfig.cs
+++ QuestJounal.MVC/App_Start/IdentityConfig.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using QuestJounal.MVC.Data;
 using QuestJounal.MVC.Models;
 
 namespace QuestJounal.MVC
