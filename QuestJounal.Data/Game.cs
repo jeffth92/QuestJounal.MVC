e0b09af79f0b542bc820393b530a74ff293b06ba
Jeffrey Hackett <Jeffth92@gmail.com>
2021-10-28 18:51:24 -0400

    prepped Data Layer

---
+++ QuestJounal.Data/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestJounal.Data
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        public string GameName { get; set; }
+        public string Location { get; set; }
+        public string GMEmail { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime JoinDate { get; set; } //might belong better in Player class.
+        public bool IsActive { get; set; }
+        [ForeignKey] // needs fixing, as will all fkey in init
+        public int AdminId { get; set; }
+        [ForeignKey]
+        public List<Player> PlayerRoster { get; set; }
+    }
+}
